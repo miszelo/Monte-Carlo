1b3bfe84416b651239d9d25d5c4d4888e7663294
michal <michal.kawczak@gmail.com>
2022-04-09 16:05:29 +0200

    Poprawki

--- Z_montecarlo.py
+++ Z_montecarlo.py
@@ -1,35 +1,48 @@
 import random
 import math
 
-while True:
-    try:
-        r = float(input("Podaj promień r > 0 : "))
-        if r > 0:
-            break
-    except:
-        print("To musi być liczba!")
-        continue
-while True:
-    try:
-        number_of_shots = int(input("Podaj liczbę strzałów > 0 : "))
-        if number_of_shots > 0:
-            break
-    except:
-        print("To musi być liczba całkowita!")
-        continue
 
-circle = 0
-square = 0
+def get_radius():
+    while True:
+        try:
+            r = float(input("Podaj promień r > 0 : "))
+            if r > 0:
+                break
+        except:
+            print("To musi być liczba!")
+            continue
+    return r
 
-for _ in range(number_of_shots):
-    x = random.uniform(-r, r)
-    y = random.uniform(-r, r)
-    distance = math.sqrt(x**2 + y**2)
-    if distance <= r:
-        circle += 1  # dodaje 1 do trafienia w tarcze koła, jezeli odległość trafienia od środka tarczy będzie mniejsza/rowna niż promien
-    square += 1  # dodaje 1 do trafienia w tarcze kwadratu zawsze, ponieważ zawsze trafia się w tarcze kwadratu
 
-pi = 4 * (circle / square) # rownie dobrze można napisac 4 * (circle/number_of_shots)
-error = abs(pi-math.pi)/pi*100
-print(f"""Na podstawie {number_of_shots} wartości losowych, dla figur w przedziale r równym: {r}, wyznaczono przybliżenie liczby pi, które wynosi: {pi}
-Różnica pomiedzy wartością liczby pi wyznaczoną metodą Monte Carlo, a wartością rzeczywistą to: {error:.2f}%""")
+def get_number_of_shots():
+    while True:
+        try:
+            number_of_shots = int(input("Podaj liczbę strzałów > 0 : "))
+            if number_of_shots > 0:
+                break
+        except:
+            print("To musi być liczba całkowita!")
+            continue
+    return number_of_shots
+
+
+def count_shots(radius, number_of_shots):
+    shot_in_circle = 0
+    for _ in range(number_of_shots):
+        x = random.uniform(-radius, radius)
+        y = random.uniform(-radius, radius)
+        distance = math.sqrt(x**2 + y**2)
+        if distance <= radius:
+            shot_in_circle += 1
+    return shot_in_circle
+
+
+if __name__ == "__main__":
+    radius = get_radius()
+    number_of_shots = get_number_of_shots()
+
+    estimated_pi = 4 * (count_shots(radius, number_of_shots) / number_of_shots)
+
+    deviation = abs(estimated_pi-math.pi)/estimated_pi*100
+
+    print(f"Na podstawie {number_of_shots} wartości losowych, dla figur w przedziale r równym: {radius}, wyznaczono przybliżenie liczby pi, które wynosi: {estimated_pi}\nRóżnica pomiedzy wartością liczby pi wyznaczoną metodą Monte Carlo, a wartością rzeczywistą to: {deviation:.2f}%")
